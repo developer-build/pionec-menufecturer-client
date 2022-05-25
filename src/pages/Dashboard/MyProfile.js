78c37f2365deb7e2943dc96d93c9f8823d433142
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-05-26 01:48:49 +0600

    my profile functionality done

--- src/pages/Dashboard/MyProfile.js
+++ src/pages/Dashboard/MyProfile.js
@@ -11,39 +11,60 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import defaultImg from "../../assests/imges/cartoon.png";
 import auth from "../../shared/firebase.init";
 import ProfileUpdateModal from "./ProfileUpdateModal";
-import fb from '../../assests/imges/socialIcon/icon3.jpg';
-import insta from '../../assests/imges/socialIcon/icon1.png';
-import linkdin from '../../assests/imges/socialIcon/icon2.png';
-import github from '../../assests/imges/socialIcon/icon4.png';
+import fb from "../../assests/imges/socialIcon/icon3.jpg";
+import insta from "../../assests/imges/socialIcon/icon1.png";
+import linkdin from "../../assests/imges/socialIcon/icon2.png";
+import github from "../../assests/imges/socialIcon/icon4.png";
+import Spinner from "../../shared/Spinner";
+import { useQuery } from "react-query";
 const MyProfile = () => {
   const [user] = useAuthState(auth);
   const [openModal, setOpenModal] = useState(false);
+  const {
+    isLoading,
+    data: currentUser,
+    refetch,
+  } = useQuery("users", () =>
+    fetch(`http://localhost:4000/current-user?email=${user?.email}`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Spinner></Spinner>;
+  }
+
   return (
     <div className="bg-gray-300 h-full">
-      <div className="px-5 text-accent">
+      <div className="px-5 bg-gray-300 text-accent">
         {/* <h1 className="text-4xl font-koulen pt-5 pb-10 uppercase text-center">
           {" "}
           My Profile
         </h1> */}
-        <div className="w-5/6 mx-auto">
+        <div className="w-5/6  bg-gray-300 h-full pb-10 mx-auto">
           <div className=" pt-10 flex-col ">
             <div class="avatar online">
               <div class="w-32 rounded-full">
                 <img src={defaultImg} alt="" />
               </div>
             </div>
-            <h1 className="text-3xl font-semibold text-gray-700 font-koulen mt-5 ">{user?.displayName}</h1>
+            <h1 className="text-3xl font-semibold text-gray-700 font-koulen mt-5 ">
+              {user?.displayName}
+            </h1>
             <p className="flex">
               {" "}
               <MailIcon className="w-6 h-6 mr-3"></MailIcon>
-              <span>{user.email}</span>
+              <span>{user?.email}</span>
             </p>
           </div>
           <div>
             <div className="flex justify-between  mx-auto mt-10">
               <div className="font-koulen  ">
                 <h2 className="text-2xl font-semibold text-gray-700 ">
-                   Information
+                  Information
                 </h2>
                 {/* <p className="mt-3 flex text-[15px] items-center">
                   <UserIcon className="w-6 h-6 mr-3  text-slate-800"></UserIcon>{" "}
@@ -55,15 +76,15 @@ const MyProfile = () => {
                 </p>
                 <p className="mt-2 flex text-[15px] items-center">
                   <PhoneIcon className="w-6 h-6 mr-3  text-slate-800"></PhoneIcon>{" "}
-                  +8801900-000000
+                  +88-{currentUser.phone ? currentUser.phone :12345678}
                 </p>
                 <p className="mt-2 flex text-[15px] items-center">
                   <MapIcon className="w-6 h-6 mr-3  text-slate-800"></MapIcon>{" "}
-                  Ashuliya,Savar,Dhaka.
+                  {currentUser.address ? currentUser.address :"No address given"}
                 </p>
                 <p className="mt-2 flex text-[15px] items-center">
                   <BookOpenIcon className="w-6 h-6 mr-3 text-slate-800"></BookOpenIcon>{" "}
-                  Diploma in Computer Sience
+                  {currentUser.study ? currentUser.study :"Study info not given"}
                 </p>
               </div>
               <div className="font-koulen">
@@ -73,43 +94,44 @@ const MyProfile = () => {
                 <p className="mt-3 flex items-center">
                   <img src={fb} className="w-6 mr-4 rounded-full" alt="" />
                   <span className="text-sm italic">
-                  https://www.facebook.com/muntasir.ahmed.naim
+                    {currentUser.fb ? currentUser.fb:'Facebook info not given'}
                   </span>
                 </p>
                 <p className="mt-2 flex items-center">
                   <img src={insta} className="w-6 mr-4 rounded-full" alt="" />
                   <span className="text-sm italic">
-                  https://www.facebook.com/muntasir.ahmed.naim
+                  {currentUser.insta ? currentUser.insta:'Instagram info not given'}
                   </span>
                 </p>
                 <p className="mt-2 flex items-center">
                   <img src={linkdin} className="w-6 mr-4 rounded-full" alt="" />
                   <span className="text-sm italic">
-                  https://www.facebook.com/muntasir.ahmed.naim
+                  {currentUser.linkdin ? currentUser.linkdin:'Linkdin info not given'}
                   </span>
                 </p>
                 <p className="mt-2 flex items-center">
                   <img src={github} className="w-6 mr-4 rounded-full" alt="" />
                   <span className="text-sm italic">
-                  https://www.facebook.com/muntasir.ahmed.naim
+                  {currentUser.github ? currentUser.github:'Github info not given'}
                   </span>
                 </p>
-               
               </div>
             </div>
             <div className="m-auto mt-10">
               <label
                 onClick={() => setOpenModal(true)}
-                for="my-modal-20"
+                for="update-profile"
                 class="btn modal-button"
               >
-               Update profile
+                Update profile
               </label>
             </div>
           </div>
         </div>
       </div>
-      {openModal && <ProfileUpdateModal setOpenModal={setOpenModal} />}
+      {openModal && (
+        <ProfileUpdateModal refetch={refetch} setOpenModal={setOpenModal} />
+      )}
     </div>
   );
 };
--- src/pages/Dashboard/ProfileUpdateModal.js
+++ src/pages/Dashboard/ProfileUpdateModal.js
@@ -1,24 +1,110 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
+import auth from "../../shared/firebase.init";
 
-const ProfileUpdateModal = ({ setOpenModal }) => {
+const ProfileUpdateModal = ({ setOpenModal, refetch }) => {
+  const [user] = useAuthState(auth);
+  const email = user?.email;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const phone = event.target.phone.value;
+    const address = event.target.address.value;
+    const study = event.target.study.value;
+    const fb = event.target.fb.value;
+    const insta = event.target.insta.value;
+    const linkdin = event.target.linkdin.value;
+    const github = event.target.github.value;
+    const data = { phone, address, study, fb, insta, linkdin, github };
+    if (phone || address || study || fb || insta || linkdin || github) {
+      fetch(`http://localhost:4000/current-user/${email}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            refetch();
+            toast.success("Profile Updated Successfuly");
+            setOpenModal(false);
+          }
+        });
+
+      event.target.reset();
+    }
+  };
   return (
     <div>
-      <input type="checkbox" id="my-modal-20" class="modal-toggle" />
-      <div class="modal">
-        <div class="modal-box relative">
+      <input type="checkbox" id="update-profile" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box md:w-4/12 mt-10 w-10/12 mx-auto relative">
           <label
-            for="my-modal-20"
-            class="btn btn-sm btn-circle absolute right-2 top-2"
+            htmlFor="update-profile"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
           >
             ✕
           </label>
-          <h3 class="text-lg font-bold">
-            Congratulations random Interner user!
+          <h3 className="text-xl font-koulen text-center text-primary font-bold mb-4">
+            Update Profile
           </h3>
-          <p class="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col font-koulen space-y-2">
+              <input
+                type="text"
+                name="address"
+                placeholder="Address"
+                className="input input-bordered w-full bg-accent "
+              />
+              <input
+                name="study"
+                type="text"
+                placeholder="Study Information"
+                className="input input-bordered w-full bg-accent"
+              />
+              <input
+                type="text"
+                name="phone"
+                placeholder="Phone Number"
+                className="input input-bordered w-full bg-accent"
+              />
+              <input
+                name="fb"
+                type="text"
+                placeholder="Facebook Link"
+                className="input input-bordered w-full bg-accent"
+              />
+
+              <input
+                name="insta"
+                type="text"
+                placeholder="Instagram Link"
+                className="input input-bordered w-full bg-accent"
+              />
+
+              <input
+                name="linkdin"
+                type="text"
+                placeholder="Linkdin Link"
+                className="input input-bordered w-full bg-accent"
+              />
+              <input
+                name="github"
+                type="text"
+                placeholder="Github Link"
+                className="input input-bordered w-full bg-accent"
+              />
+
+              <input
+                type="submit"
+                className="btn btn-primary "
+                value="Submit"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
