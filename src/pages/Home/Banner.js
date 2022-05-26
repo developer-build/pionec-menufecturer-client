1131ef8f6b9ba928b6067192838b386de390fc71
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-05-26 06:25:26 +0600

    header text change

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
       <Navber />
       <div className="h-[68vh] flex flex-col gap-5 justify-center items-center">
         <h1 className="text-4xl md:text-6xl text-center font-semibold text-gray-300 font-koulen uppercase">
-          Best online Durable Paiting <br /> Tools menufecturer
+          Best online Creative Paiting <br /> Tools menufecturer
         </h1>
         <button
           className="text-accent uppercase bg-orange-600  py-2 px-8  rounded-md font-bold flex justify-between items-center"
