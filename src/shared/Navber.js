ddbe3a4b556d39b40a1c1d84602351e9ccc913c4
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-05-26 07:15:00 +0600

    navber style change

--- src/shared/Navber.js
+++ src/shared/Navber.js
@@ -4,7 +4,6 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "./firebase.init";
 import { signOut } from "firebase/auth";
-import CustomLink from "./CustomLink";
 
 const Navber = () => {
   const [open, setOpen] = useState(false);
@@ -37,46 +36,46 @@ const Navber = () => {
             )}
           </div>
           <div
-            className={`flex-1 flex flex-col  md:items-center  p-4 md:p-0 rounded-b-lg  md:flex-row justify-end  transition-all duration-500 ease-in-out  absolute md:static   md:z-auto z-[2] right-0 md:w-full  space-y-2 md:space-y-0 ${
+            className={`flex-1 flex flex-col  md:items-center  p-4 md:p-0 rounded-b-lg  md:flex-row justify-end  transition-all duration-500 ease-in-out  absolute md:static   md:z-auto z-[2] right-0 w-full  space-y-2 md:space-y-0 ${
               open
-                ? "top-16 opacity-100 bg-white md:bg-neutral transition-all ease-out"
+                ? "top-16 opacity-100 bg-white md:bg-transparent transition-all ease-out"
                 : "top-[-490px] md:opacity-100 opacity-0 transition-all ease-in"
             }`}
           >
-            <CustomLink
+            <Link
               to="/"
               className=" px-5 font-semibold rounded py-1 text-black md:text-white "
             >
               Home
-            </CustomLink>
+            </Link>
             {user && (
-              <CustomLink
+              <Link
                 to="/dashboard/my-profile"
                 className=" px-5 font-semibold rounded py-1 text-black md:text-white"
               >
                 Dashboard
-              </CustomLink>
+              </Link>
             )}
 
-            <CustomLink
+            <Link
               to="/blogs"
               className=" px-5 font-semibold rounded py-1 text-black md:text-white "
             >
               Blogs
-            </CustomLink>
-            <CustomLink
+            </Link>
+            <Link
               to="/contact"
               className=" px-5 font-semibold rounded py-1 text-black md:text-white "
             >
               Contact
-            </CustomLink>
+            </Link>
 
-            <CustomLink
+            <Link
               to="/my-portfolio"
               className=" px-5 font-semibold rounded py-1 text-black md:text-white "
             >
               Portfolio
-            </CustomLink>
+            </Link>
             {user ? (
               <div className="dropdown self-center dropdown-end  ">
                 <label
@@ -90,14 +89,14 @@ const Navber = () => {
                   className="dropdown-content menu space-y-2 rounded-lg top-8  px-2 py-4 shadow bg-base-100  w-44 "
                 >
                   <li>
-                    <CustomLink
+                    <Link
                       to="/dashboard/my-profile"
-                      className="bg-secondary rounded-lg text-accent flex items-center justify-center "
+                      className="btn btn-secondary text-accent rounded-md font-bold"
                     >
                       {user.displayName}
-                    </CustomLink>
+                    </Link>
                   </li>
-                  <li>
+                  <li className="">
                     <button
                       className="px-5 font-semibold rounded py-2 flex items-center justify-center  bg-orange-600 text-gray-900"
                       onClick={() => {
@@ -111,12 +110,12 @@ const Navber = () => {
                 </ul>
               </div>
             ) : (
-              <CustomLink
+              <Link
                 to="/login"
                 className=" px-5 font-semibold rounded py-2  bg-orange-600 text-gray-900"
               >
                 Login
-              </CustomLink>
+              </Link>
             )}
             {/* <button
               className=" px-4 font-semibold rounded py-1  bg-orange-600 text-gray-900"
